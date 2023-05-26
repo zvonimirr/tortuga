9bccce02069f917b128d1022eea09a785f625370
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-05-26 04:38:20 +0200

    feat: force 405 on invalid methods

--- lib/tortuga/plugs/normalize_method.ex
+++ lib/tortuga/plugs/normalize_method.ex
@@ -1,8 +1,12 @@
 defmodule Tortuga.Plugs.NormalizeMethod do
   @moduledoc """
-  A plug that normalizes the HTTP method to uppercase.
+  A plug that normalizes the HTTP method to uppercase, as well as
+  returning a 405 Method Not Allowed response if the method is
+  something weird.
   """
 
+  require Logger
+  import Plug.Conn, only: [put_resp_header: 3, send_resp: 3, halt: 1]
   @behaviour Plug
 
   @impl true
@@ -15,6 +19,36 @@ defmodule Tortuga.Plugs.NormalizeMethod do
   end
 
   defp normalize_method(conn) do
-    %{conn | method: String.upcase(conn.method)}
+    method = String.upcase(conn.method)
+
+    # Check if the method isn't something weird.
+    if method in ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"] do
+      %{conn | method: method}
+    else
+      # Return a 405 Method Not Allowed response.
+      log_invalid_method(conn)
+
+      conn
+      |> put_resp_header("allow", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS")
+      |> send_resp(:method_not_allowed, "Invalid method: #{method}")
+      |> halt()
+    end
+  end
+
+  defp log_invalid_method(conn) do
+    ip = conn.remote_ip |> :inet.ntoa() |> to_string()
+
+    [
+      :color33,
+      "(#{ip})",
+      :reset,
+      " ---> ",
+      :color31,
+      "INVALID METHOD ",
+      :yellow,
+      "'#{conn.method}'"
+    ]
+    |> Bunt.ANSI.format()
+    |> Logger.warn()
   end
 end
