20913917ff2a752199a53b5be591bcbbecf1c918
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-05-26 03:12:08 +0200

    feat: initial commit

---
+++ lib/tortuga.ex
@@ -0,0 +1,18 @@
+defmodule Tortuga do
+  @moduledoc """
+  Documentation for `Tortuga`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tortuga.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
