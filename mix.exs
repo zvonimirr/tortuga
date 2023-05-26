3bff16f04a72b61539c8907cb312cfe3282cf9b3
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-05-26 03:17:49 +0200

    feat: setup exdoc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Tortuga.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Tortuga",
+      source_url: "https://github.com/zvonimirr/tortuga",
+      homepage_url: "https://github.com/zvonimirr/tortuga",
+      docs: [
+        main: "Tortuga",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -22,8 +31,7 @@ defmodule Tortuga.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
